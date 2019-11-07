4cae70ae8b853ac6f2cfba1339473d9b2e0907bf
VictorHCord <victorhugocordeiro1997@gmail.com>
2019-11-07 01:12:14 -0300

    Finally firts project react

--- components/AllPost.js
+++ components/AllPost.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function AllPost({ post }) {
-  const {author , date , comment} = post;
+  const {author , date , content, comments } = post;
 
   return (
     <div className="container">
@@ -9,10 +9,29 @@ function AllPost({ post }) {
       <img src={author.avatar} />
       <span className="date">{date}</span>
       <span className="name">{author.name}</span>
+      <p className="content">{content}</p>
+      <PostComments comments={comments} />
       </div>
     </div>
-
   );
 }
+
+function PostComments({ comments }) {
+  return (
+    <div className="post-comments">
+      <div className="divider" />
+      {comments.map(comment => (
+        <div key={comment.id} className="comment">
+          <img className="avatar" src={comment.author.avatar} />
+          <p>
+            <span>{comment.author.name}</span>
+            {comment.content}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 
 export default AllPost;
--- components/PostList.js
+++ components/PostList.js
@@ -7,8 +7,8 @@ class PostList extends Component {
       {
         id: 1,
         author: {
-          name: "Julio Alcantara",
-          avatar: "https://pickaface.net/gallery/avatar/20151107_070622_3057_aleatorio.png"
+          name: "Victor Cordeiro",
+          avatar: "https://avatars3.githubusercontent.com/u/20667324?s=400&u=49c5b001d678d043462670b423164a6128430712&v=4"
         },
         date: "04 Jun 2019",
         content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
@@ -17,40 +17,71 @@ class PostList extends Component {
             id: 1,
             author: {
               name: "Diego Fernandes",
-              avatar: "https://pickaface.net/gallery/avatar/20151107_070622_3057_aleatorio.png"
+              avatar: "https://avatars1.githubusercontent.com/u/2254731?s=400&v=4"
             },
-            content: "Conteúdo do comentário"
+            content: "Então bro, a rocketseat está sempre contratado, serio mesmo, uma boa parte da equipe da rocketseat é por ex alunos do bootcamp entra em contato lá no discord que eles te dão uma ajudinha."
           }
         ]
       },
       {
         id: 2,
         author: {
-          name: "Victor Cordeiro",
-          avatar: "https://pickaface.net/gallery/avatar/20151107_070622_3057_aleatorio.png"
+          name: "Luiz Fernando",
+          avatar: "https://avatars0.githubusercontent.com/u/32375042?s=400&v=4"
         },
         date: "04 novembro 2019",
-        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        content: "E ae o projeto está ou não funcionando? hahaha quero logo testar isso ae",
         comments: [
           {
             id: 2,
             author: {
-              name: "Leonardo Fernandes",
-              avatar: "https://pickaface.net/gallery/avatar/20151107_070622_3057_aleatorio.png"
+              name: "Diego Fernandes",
+              avatar: "https://avatars1.githubusercontent.com/u/2254731?s=400&v=4"
             },
-            content: "Conteúdo do comentário"
+            content: "Cara o projeto tá a mil, da uma olhada lá no repositorio tu vai curtir demais kk"
+          },
+          {
+            id: 3,
+            author: {
+              name: "Victor Cordeiro",
+              avatar: "https://avatars3.githubusercontent.com/u/20667324?s=400&u=49c5b001d678d043462670b423164a6128430712&v=4"
+            },
+            content : "Então cara to curtindo fazer esse projeto hahah ta divertido"
           }
         ]
       },
+
+      {
+        id: 3,
+        author: {
+          name: "Victor Cordeiro",
+          avatar: "https://avatars3.githubusercontent.com/u/20667324?s=400&u=49c5b001d678d043462670b423164a6128430712&v=4"
+        },
+        date: "15 dezembro 2019",
+        content: "Esse bootcamp está sensacional, as pessoas deveriam testar isto está ficando cada vez mais interessantes.",
+        comments: [
+          {
+            id: 3,
+            author: {
+              name: "Luiz Fernando",
+              avatar: "https://avatars0.githubusercontent.com/u/32375042?s=400&v=4"
+            },
+            content: "Oque está aprendendo agora lá cara? "
+          },
+          
+        ]
+      },
+
+
     ]
   };
 
   render() {
-    const { posts } = this.state;
-
+    const { posts } = this.state;   
     return (
       <>
         {posts.map(post => <AllPost key={post.id} post={post} />)}
+
       </>
     );
   }
